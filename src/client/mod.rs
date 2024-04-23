9aa2597bf19889c897f94fb7f8d54aecb1c282d6
Henrik Antonsson <henrik.antonsson@gmail.com>
2024-04-23 15:51:22 +0200

    Provide access token in send

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -2,7 +2,6 @@ pub(crate) mod response;
 
 use crate::client::response::{ErrorReason, FcmError, FcmResponse, RetryAfter};
 use crate::{Message, MessageInternal};
-use gauth::serv_account::ServiceAccount;
 use reqwest::header::RETRY_AFTER;
 use reqwest::{Body, StatusCode};
 use serde::Serialize;
@@ -10,12 +9,6 @@ use serde::Serialize;
 /// An async client for sending the notification payload.
 pub struct Client {
     http_client: reqwest::Client,
-}
-
-impl Default for Client {
-    fn default() -> Self {
-        Self::new()
-    }
 }
 
 // will be used to wrap the message in a "message" field
@@ -42,92 +35,10 @@ impl Client {
         Client { http_client }
     }
 
-    fn get_service_key_file_name(&self) -> Result<String, String> {
-        let key_path = match dotenv::var("GOOGLE_APPLICATION_CREDENTIALS") {
-            Ok(key_path) => key_path,
-            Err(err) => return Err(err.to_string()),
-        };
-
-        Ok(key_path)
-    }
-
-    fn read_service_key_file(&self) -> Result<String, String> {
-        let key_path = self.get_service_key_file_name()?;
-
-        let private_key_content = match std::fs::read(key_path) {
-            Ok(content) => content,
-            Err(err) => return Err(err.to_string()),
-        };
-
-        Ok(String::from_utf8(private_key_content).unwrap())
-    }
-
-    fn read_service_key_file_json(&self) -> Result<serde_json::Value, String> {
-        let file_content = match self.read_service_key_file() {
-            Ok(content) => content,
-            Err(err) => return Err(err),
-        };
-
-        let json_content: serde_json::Value = match serde_json::from_str(&file_content) {
-            Ok(json) => json,
-            Err(err) => return Err(err.to_string()),
-        };
-
-        Ok(json_content)
-    }
-
-    fn get_project_id(&self) -> Result<String, String> {
-        let json_content = match self.read_service_key_file_json() {
-            Ok(json) => json,
-            Err(err) => return Err(err),
-        };
-
-        let project_id = match json_content["project_id"].as_str() {
-            Some(project_id) => project_id,
-            None => return Err("could not get project_id".to_string()),
-        };
-
-        Ok(project_id.to_string())
-    }
-
-    async fn get_auth_token(&self) -> Result<String, String> {
-        let tkn = match self.access_token().await {
-            Ok(tkn) => tkn,
-            Err(_) => return Err("could not get access token".to_string()),
-        };
-
-        Ok(tkn)
-    }
-
-    async fn access_token(&self) -> Result<String, String> {
-        let scopes = vec!["https://www.googleapis.com/auth/firebase.messaging"];
-        let key_path = self.get_service_key_file_name()?;
-
-        let mut service_account = ServiceAccount::from_file(&key_path, scopes);
-        let access_token = match service_account.access_token().await {
-            Ok(access_token) => access_token,
-            Err(err) => return Err(err.to_string()),
-        };
-
-        let token_no_bearer = access_token.split(char::is_whitespace).collect::<Vec<&str>>()[1];
-
-        Ok(token_no_bearer.to_string())
-    }
-
-    pub async fn send(&self, message: Message) -> Result<FcmResponse, FcmError> {
+    pub async fn send(&self, access_token: &str, project_id: &str, message: Message) -> Result<FcmResponse, FcmError> {
         let fin = message.finalize();
         let wrapper = MessageWrapper::new(&fin);
         let payload = serde_json::to_vec(&wrapper).unwrap();
-
-        let project_id = match self.get_project_id() {
-            Ok(project_id) => project_id,
-            Err(err) => return Err(FcmError::ProjectIdError(err)),
-        };
-
-        let auth_token = match self.get_auth_token().await {
-            Ok(tkn) => tkn,
-            Err(err) => return Err(FcmError::ProjectIdError(err)),
-        };
 
         // https://firebase.google.com/docs/reference/fcm/rest/v1/projects.messages/send
         let url = format!("https://fcm.googleapis.com/v1/projects/{}/messages:send", project_id);
@@ -136,7 +47,7 @@ impl Client {
             .http_client
             .post(&url)
             .header("Content-Type", "application/json")
-            .bearer_auth(auth_token)
+            .header("Authorization", access_token)
             .body(Body::from(payload))
             .build()?;
 
